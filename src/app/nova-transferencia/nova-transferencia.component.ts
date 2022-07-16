c1c36de5b8bb7b81b800778ad66b9e00caaedd6f
henrique araujo <henriquedotdev@gmail.com>
2022-07-16 12:24:38 -0300

    transferencia com data biding

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent implements OnInit {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  transferir() {
+    console.log('nova transferencia solicitada');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limpatCampos();
+  }
+  limpatCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
